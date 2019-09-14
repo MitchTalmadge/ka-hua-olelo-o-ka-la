4e45f961bfef2d3e0f8a44d14ed2a3ceb49ea5b6
Mitch Talmadge <mitch@mitchtalmadge.com>
2019-09-14 02:31:40 -0600

    Add example twitter code

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,18 @@
+import Twitter = require("twitter");
+
 export async function handler (event: any, context: AWSLambda.Context) {
+
+    let client = new Twitter({
+        consumer_key: '',
+        consumer_secret: '',
+        access_token_key: '',
+        access_token_secret: ''
+    });
+
+    client.get('statuses/user_timeline', {}, (error, tweets, response) => {
+
+    });
+
     return {
         statusCode: 200,
         body: JSON.stringify('Hello from Lambda!'),
