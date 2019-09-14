c1544f51d409406978e2ec88c7e9430e33941491
Mitch Talmadge <mitch@mitchtalmadge.com>
2019-09-14 02:09:02 -0600

    Change from callback to async function

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export function handler (event: any, context: AWSLambda.Context, callback: AWSLambda.Callback) {
+export async function handler (event: any, context: AWSLambda.Context) {
     return {
         statusCode: 200,
         body: JSON.stringify('Hello from Lambda!'),
